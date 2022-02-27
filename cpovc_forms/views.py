fc55ff4463ef1d805e809f89caf1448a03320adc
Vincent Kamemia <kamemia18@yahoo.com>
2022-02-27 09:32:38 +0300

    updated urls.py

--- cpovc_forms/views.py
+++ cpovc_forms/views.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import reverse
+from django.url import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib import messages
